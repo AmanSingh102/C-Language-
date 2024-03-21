ca62db562be7c8eaf48d00057cafb0879c1cc7f1
Aman Singh <161552125+AmanSingh102@users.noreply.github.com>
2024-03-21 09:12:34 +0530

    Add files via upload

---
+++ 1 Basic Start/02_Sum_program.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a,b;
+    printf("Enter 1st number : ");
+    scanf("%d",&a);
+    printf("Enter 2nd number : ");
+    scanf("%d",&b);
+
+    printf("Sum of %d and %d is %d",a,b,a+b);
+    getch();
+}
